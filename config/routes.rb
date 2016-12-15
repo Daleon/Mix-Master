a2cc0cadbabe8bb196bd708163746556dd31b7bc
Daleon <rugfia@yahoo.com>
2016-12-14 21:13:44 -0700

    I-4 Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :artists, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  # resources :artists, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   resources :artists do
     resources :songs, only: [:new, :create]
   end
-  
-  resources :songs, only: [:show]
+
+  resources :songs, only: [:show, :edit, :update]
+  resources :playlists, except: [:delete]
 end
