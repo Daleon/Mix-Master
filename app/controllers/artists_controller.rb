0777ff23b73642c205040e90c054fd0e41203c1e
Daleon <rugfia@yahoo.com>
2016-12-14 22:03:36 -0700

    I-5 Complete

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,6 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
+
   def index
     @artists = Artist.all
   end
@@ -17,15 +19,12 @@ class ArtistsController < ApplicationController
   end
 
   def show
-    @artist = Artist.find(params[:id])
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
     @artist.update(artist_params)
     @artist.save
 
@@ -35,7 +34,6 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
     @artist.destroy
 
     redirect_to artists_path
@@ -45,5 +43,9 @@ class ArtistsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name, :image_path)
+  end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
   end
 end
